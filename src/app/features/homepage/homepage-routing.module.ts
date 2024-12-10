1b6c8874f62745409ad89397edbfa5a6d9ac0121
Steeve MOKOKO <steeve.mokoko14@gmail.com>
2024-12-10 20:29:45 +0100

    Ajout sections de la homepage, non responsive

--- src/app/features/homepage/homepage-routing.module.ts
+++ src/app/features/homepage/homepage-routing.module.ts
@@ -4,12 +4,14 @@ import { HomepageComponent } from './homepage.component';
 import { MainComponent } from './pages/main/main.component';
 import { OverviewComponent } from './pages/overview/overview.component';
 import { PageNotFoundComponent } from '../../shared/components/page-not-found/page-not-found.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomepageComponent,
     children: [
+      { path: '', component: LandingPageComponent },
       { path: 'main', component: MainComponent },
       { path: 'overview', component: OverviewComponent },
       { path: '', redirectTo: 'main', pathMatch: 'full' },
--- src/app/features/homepage/homepage.module.ts
+++ src/app/features/homepage/homepage.module.ts
@@ -6,9 +6,16 @@ import { HomepageComponent } from './homepage.component';
 import { SharedModule } from '../../shared/shared.module';
 import { MainComponent } from './pages/main/main.component';
 import { OverviewComponent } from './pages/overview/overview.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [HomepageComponent, MainComponent, OverviewComponent],
-  imports: [CommonModule, HomepageRoutingModule, SharedModule], // Importe SharedModule afin de pouvoir utiliser l'header e le footer
+  declarations: [
+    HomepageComponent, 
+    MainComponent, 
+    OverviewComponent, 
+    LandingPageComponent, 
+  ],
+  imports: [CommonModule, HomepageRoutingModule, SharedModule, FontAwesomeModule], // Importe SharedModule afin de pouvoir utiliser l'header e le footer
 })
 export class HomepageModule {}
